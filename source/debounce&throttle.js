24d469cb14f60cea0b6e7e5bdef9ab6a9d70089e
bairuobing <brb19960117@gmail.com>
2020-05-02 14:12:16 +0800

    feat: image change

---
+++ source/debounce&throttle.js
@@ -0,0 +1,22 @@
+function debounce(fn, time) {
+    let timer = null;
+    return function() {
+        clearTimeout(timer);
+        setTimeout(() => {
+            fn.apply(this,arguments);
+        },time);
+    }
+}
+
+function throttle(fn, time) {
+    let canRun = true; // 通过闭包保存一个标记
+    return function () {
+        if (!canRun) return; // 在函数开头判断标记是否为true，不为true则return
+        canRun = false; // 立即设置为false
+        setTimeout(() => { // 将外部传入的函数的执行放在setTimeout中
+            fn.apply(this, arguments);
+            // 最后在setTimeout执行完毕后再把标记设置为true(关键)表示可以执行下一次循环了。当定时器没有执行的时候标记永远是false，在开头被return掉
+            canRun = true;
+        }, time);
+    };
+}
